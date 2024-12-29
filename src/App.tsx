2581a00cb61b0ebed86e4bbbce7d7310be5f3896
민우석 <s23044@gsm.hs.kr>
2024-12-29 22:11:40 +0900

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@ import CongestionBoard from './components/CongestionBoard';
 import { DesignChart } from './components/DesignChart';
 import Header from './components/Header';
 import { Image } from './components/Image';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type congestionType = 'High' | 'Medium' | 'Low';
+
+interface Data {
+  id: number;
+  peopleCount: number;
+  status: congestionType;
+}
 
 function App() {
-  const personData = [
+  const [personData, setPersonData] = useState<Data[]>([]);
+  const persoData = [
     { person: 20 },
     { person: 413 },
     { person: 647 },
@@ -14,14 +25,35 @@ function App() {
     { person: 980 },
     { person: 841 },
   ];
-  const p = 'Low'
-  const personTick = Math.max(...personData.map((item) => item.person));
+  const personTick = Math.max(...personData.map((item) => item.peopleCount));
   const personTicks = Array.from(
     { length: 4 },
     (_, i) => (personTick * (i + 1)) / 4
   );
 
-  
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(`${import.meta.env.VITE_SERVER_URL}`);
+      if (data) {
+        setPersonData(data);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          console.log(
+            `Error: ${error.response.status} - ${error.response.data}`
+          );
+        } else {
+          console.log('Error: No response received');
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='flex flex-col items-center font-Pretendard bg-home'>
       <Header />
@@ -30,14 +62,16 @@ function App() {
       </div>
       <section className='flex gap-10 py-[57px]'>
         <div className='flex flex-col gap-[23px]'>
-          <CongestionBoard congestion={p} />
+          <CongestionBoard
+            congestion={personData.length > 11 ? personData[11].status : 'Low'}
+          />
           <TimeBoard />
         </div>
         <DesignChart
           title='사람'
-          congestion={p}
-          person={personData[5].person}
-          data={personData}
+          congestion={personData.length > 11 ? personData[11].status : 'Low'}
+          person={personData.length > 11 ? personData[11].peopleCount : 11}
+          data={persoData}
           tick={personTick}
           ticks={personTicks}
         />
--- src/components/DesignChart.tsx
+++ src/components/DesignChart.tsx
@@ -1,12 +1,6 @@
 'use client';
 
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  YAxis,
-} from 'recharts';
+import { CartesianGrid, Line, LineChart, YAxis } from 'recharts';
 
 import {
   Card,
@@ -35,7 +29,7 @@ interface Props {
 
 const chartConfig = {
   desktop: {
-    label: 'person',
+    label: 'Person',
     color: 'hsl(var(--chart-1))',
   },
 } satisfies ChartConfig;
@@ -48,62 +42,66 @@ export function DesignChart({
   tick,
   ticks,
 }: Props) {
-  const [color, setColor] = useState<string>('green');
+  const [color, setColor] = useState<string>('');
 
   useEffect(() => {
-    switch(congestion){
+    switch (congestion) {
       case 'High':
-        setColor('red');
+        setColor('#DC1111');
         break;
       case 'Medium':
-        setColor('yellow');
+        setColor('#F1D84C');
         break;
       case 'Low':
-        setColor('green');
+        setColor('#60C000');
         break;
     }
-  }, [congestion]);
+  }, [congestion, data]);
 
   return (
     <Card className='w-[590px] h-[431px] px-8 py-7'>
       <CardHeader>
         <CardTitle className='text-[30px] !font-light'>{title}</CardTitle>
         <CardDescription className='text-[34px] flex items-center font-light'>
-          <span className={`text-[50px] font-semibold !text-[${color}]`}>{person}</span>명
+          <span
+            className={`text-[50px] font-semibold`}
+            style={{ color: color }}
+          >
+            {person}
+          </span>
+          명
         </CardDescription>
       </CardHeader>
       <CardContent className='pt-2'>
         <ChartContainer config={chartConfig} className='w-[518px] h-[220px]'>
-          <ResponsiveContainer width='100%' height={250}>
-            <LineChart accessibilityLayer data={data}>
-              <CartesianGrid vertical={false} horizontal={true} />
-              <YAxis
-                tickLine={false}
-                tickMargin={30}
-                axisLine={false}
-                domain={[0, `${tick}`]}
-                ticks={ticks && [0, ...ticks]}
-                tick={{ color: '#000000', fontSize: '13px', fontWeight: '500' }}
-                interval={0}
-              />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Line
-                dataKey='person'
-                type='linear'
-                stroke='blue'
-                strokeWidth={2}
-                dot={{
-                  fill: 'blue',
-                }}
-                activeDot={{
-                  r: 6,
-                }}
-              />
-            </LineChart>
-          </ResponsiveContainer>
+          <LineChart accessibilityLayer data={data}>
+            <CartesianGrid vertical={false} horizontal={true} />
+            <YAxis
+              tickLine={false}
+              tickMargin={30}
+              axisLine={false}
+              domain={[0, `${tick}`]}
+              ticks={ticks && [0, ...ticks]}
+              tick={{ color: '#000000', fontSize: '13px', fontWeight: '500' }}
+              interval={0}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey='person'
+              type='linear'
+              stroke='blue'
+              strokeWidth={2}
+              dot={{
+                fill: 'blue',
+              }}
+              activeDot={{
+                r: 6,
+              }}
+            />
+          </LineChart>
         </ChartContainer>
       </CardContent>
     </Card>
