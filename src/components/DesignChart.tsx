1838f37699645d3853d8d4eb1c7cc36242e5ed9e
민우석 <s23044@gsm.hs.kr>
2024-12-30 00:43:20 +0900

    fix chart

--- src/components/DesignChart.tsx
+++ src/components/DesignChart.tsx
@@ -18,31 +18,46 @@ import {
 } from '@/components/ui/chart';
 import { useEffect, useState } from 'react';
 
+type congestionType = 'High' | 'Medium' | 'Low';
+
+interface Data {
+  id: number;
+  peopleCount: number;
+  status: congestionType;
+}
+
 interface Props {
   title: string;
   congestion: 'High' | 'Medium' | 'Low';
   person?: number;
-  data: object[];
-  tick?: number;
-  ticks?: number[];
+  data: Data[];
 }
 
 const chartConfig = {
   desktop: {
-    label: 'Person',
+    label: 'peopleCount',
     color: 'hsl(var(--chart-1))',
   },
 } satisfies ChartConfig;
 
-export function DesignChart({
-  title,
-  congestion,
-  person,
-  data,
-  tick,
-  ticks,
-}: Props) {
+export function DesignChart({ title, congestion, person, data }: Props) {
   const [color, setColor] = useState<string>('');
+  const [tick, setTick] = useState<number[]>([0, 1000]);
+  const [ticks, setTicks] = useState<number[]>([0, 1000]);
+
+  const fetchTick = () => {
+    const people = Math.max(...data.map((item) => item.peopleCount));
+    console.log(people);
+    setTick([0, people]);
+  };
+
+  useEffect(() => {
+    fetchTick();
+  }, []);
+
+  useEffect(() => {
+    setTicks([0, tick[1] / 4, tick[1] / 2, (tick[1] * 3) / 4, tick[1]]);
+  }, [tick]);
 
   useEffect(() => {
     switch (congestion) {
@@ -56,7 +71,7 @@ export function DesignChart({
         setColor('#60C000');
         break;
     }
-  }, [congestion, data]);
+  }, [congestion]);
 
   return (
     <Card className='w-[590px] h-[431px] px-8 py-7'>
@@ -80,8 +95,8 @@ export function DesignChart({
               tickLine={false}
               tickMargin={30}
               axisLine={false}
-              domain={[0, `${tick}`]}
-              ticks={ticks && [0, ...ticks]}
+              domain={tick}
+              ticks={ticks}
               tick={{ color: '#000000', fontSize: '13px', fontWeight: '500' }}
               interval={0}
             />
@@ -90,7 +105,7 @@ export function DesignChart({
               content={<ChartTooltipContent hideLabel />}
             />
             <Line
-              dataKey='person'
+              dataKey='peopleCount'
               type='linear'
               stroke='blue'
               strokeWidth={2}
