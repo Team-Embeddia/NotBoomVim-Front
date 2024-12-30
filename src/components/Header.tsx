7c448ffd151a5015bb80c34272b9e9d33a3766ce
민우석 <s23044@gsm.hs.kr>
2024-12-30 09:55:44 +0900

    change src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ const Header = () => {
   return (
     <header className='sticky top-0 z-50 flex items-center w-full h-16 px-5 py-4 bg-black'>
       <div className='flex gap-5'>
-        <img src='/src/asset/logo.png' className='w-9 h-9 ' />
+        <img src='https://mirror.zzunipark.com/_notboomvim/NotBoomVim-Front/src/asset/logo.png' className='w-9 h-9 ' />
         <div className='flex items-center gap-[7px]'>
-          <img src='/src/asset/text.png' className='w-[120px] h-5' />
+          <img src='https://mirror.zzunipark.com/_notboomvim/NotBoomVim-Front/src/asset/text.png' className='w-[120px] h-5' />
           <p className='text-[24px] text-white'>CONTROL PANEL</p>
         </div>
       </div>
